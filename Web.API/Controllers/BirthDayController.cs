2c2e52dcb107e8b846ed09e71a4a3d279e9dd009
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2021-09-11 16:11:48 -0300

    Add new extensions methods - DateTime

---
+++ Web.API/Controllers/BirthDayController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.API.Extension.Methods;
+
+namespace Web.API.Controllers
+{
+    [Route("v1/api")]
+    public class BirthDayController : Controller
+    {
+        [HttpGet("birthday")]
+        public IActionResult Index()
+        {
+            var birthDay = new DateTime().BirthDay(04, 03, 2001);
+            return Ok(birthDay);
+        }
+    }
+}
---
+++ Web.API/Extension.Methods/Birthday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.API.Extension.Methods
+{
+    public static class Birthday
+    {
+        public static string BirthDay(this DateTime date, int dia, int mes, int ano)
+        {
+            return string.Concat(dia, mes, ano);
+        }
+    }
+}
